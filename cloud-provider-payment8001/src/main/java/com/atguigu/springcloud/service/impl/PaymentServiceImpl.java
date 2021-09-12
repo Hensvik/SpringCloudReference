d24cdbe090744205af9cfa410479d9ddbd83a801
“Jakey” <872304091@qq.com>
2021-09-12 21:08:48 +0800

    20210912

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -1,8 +1,9 @@
-package com.atguigu.springcloud;
+package com.atguigu.springcloud.service.impl;
 
 import com.atguigu.springcloud.dao.PaymentDao;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
